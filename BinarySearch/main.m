a414e1e11b40d73da697505558bcf155ed5e099a
neliSun <nelson.chow91.nc@gmail.com>
2016-03-08 19:07:44 -0800

    works only when the element searched for is within the array.

--- BinarySearch/main.m
+++ BinarySearch/main.m
@@ -8,10 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
+int findElem(int array[], int st, int en, int elem);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        int ar[] = {1,2,3,5,7,12,14,26,35};
+        int size = sizeof(ar)/sizeof(int);
+
+        int index = findElem(ar, 0, size, 3);
+        NSLog(@"%i", index);
     }
     return 0;
+}
+
+int findElem(int array[], int st, int en, int elem) {
+    
+    int center = (en + st) / 2;
+    
+    if (array[center] == elem) {
+        return center;
+    } else if (array[center] > elem) {
+        int index = findElem(array, st, center, elem);
+//        center = (center - st) / 2;
+        return index;
+    } else if (array[center] < elem){
+        int index = findElem(array, center, en, elem);
+//        center = (center + en) / 2;
+        return index;
+    }
+    
+    return -1;
+    
 }
